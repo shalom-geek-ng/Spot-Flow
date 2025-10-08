c05294473d789d595fb5dcd55e4c6707d84dfef9
shalom-geek-ng <oluwafemishalom3@gmail.com>
2025-10-08 02:14:29 +0100

    add metrics binder

--- src/main/java/com/spotflow/SpotFlow/SpotFlowApplication.java
+++ src/main/java/com/spotflow/SpotFlow/SpotFlowApplication.java
@@ -1,9 +1,12 @@
 package com.spotflow.SpotFlow;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+	    exclude = { SystemMetricsAutoConfiguration.class }
+	)
 public class SpotFlowApplication {
 
 	public static void main(String[] args) {
